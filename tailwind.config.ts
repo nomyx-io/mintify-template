dc5cf4deeb2326db565956e5dd2468c499c41cf4
divyanshjain9 <divyansh.b@rejolut.com>
2023-09-05 19:19:24 +0530

    feat : material tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss'
+// const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT"
 
-const config: Config = {
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,5 +18,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+})
+// export default config
