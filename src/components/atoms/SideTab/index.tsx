37561af2387f8795ed1d47471b222716d2e47461
Joshua Krinsky <jkrinsky@liberation-tech.com>
2024-02-10 04:23:38 -1000

    - debugging mintify

--- src/components/atoms/SideTab/index.tsx
+++ src/components/atoms/SideTab/index.tsx
@@ -2,14 +2,16 @@ import Link from 'next/link'
 import React from 'react'
 
 const SideTab = ({ icon, label, href }: any) => {
-  return (
+    return (<></>);
+
+/*  return (
     <Link href={href}>
       <div className='flex gap-4'>
         {icon && icon}
         {label}
       </div>
     </Link>
-  )
+  )*/
 }
 
 export default SideTab
