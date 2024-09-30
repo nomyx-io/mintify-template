0b33e335d0242eec60c4d2dee90bb75eaeb4ef84
Nathan Patterson <nathan.e.patterson@gmail.com>
2024-09-30 14:01:38 -0600

    Remove lender lab branding

--- src/components/molecules/TopNavBar.tsx
+++ src/components/molecules/TopNavBar.tsx
@@ -6,7 +6,8 @@ import ThemeToggle from "@/components/ThemeToggle";
 import { useAccount } from "wagmi";
 import { UserContext } from "@/pages/_app";
 import Link from "next/link";
-import logo from "@/assets/LenderLabLogo.svg";
+import logoDark from "@/assets/nomyx_logo_dark.png";
+import logoLight from "@/assets/nomyx_logo_light.png";
 
 const { Header } = Layout;
 
@@ -23,7 +24,8 @@ const TopNavBar = () => {
     <Header className="w-full p-5 flex items-center justify-between bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark">
       <div>
         <Link href={"/home"}>
-          <Image alt="" src={logo} />
+          <Image src={logoLight} alt="Logo" width={150} height={40} priority className="block dark:hidden" />
+          <Image src={logoDark} alt="Logo" width={150} height={40} priority className="hidden dark:block" />
         </Link>
       </div>
       <div className="hidden sm:flex items-center justify-end gap-5">
