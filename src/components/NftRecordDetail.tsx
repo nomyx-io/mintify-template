890f6468ca1ff08ff0d7f215a5350b3b3b16ef54
Aaron Hanks <theaaronhanks@duck.com>
2024-10-08 10:29:12 -0600

    rework mint form to have correct fields

--- src/components/NftRecordDetail.tsx
+++ src/components/NftRecordDetail.tsx
@@ -1,42 +1,106 @@
-import React, { ReactElement } from "react";
-import { Button, Card, Table, TableColumnType, Tabs } from "antd";
+import React from 'react';
+import { Button, Card, Checkbox, Table, Tabs } from 'antd';
 
-import { useRouter } from "next/router";
-import { ShareIcon } from "@/assets";
-import { LeftOutlined } from "@ant-design/icons";
-import Image from "next/image";
+import { useRouter } from 'next/router';
+import { ShareIcon } from '@/assets';
+import { LeftOutlined } from '@ant-design/icons';
+import Image from 'next/image';
+import { hashToColor } from '@/utils/colorUtils';
 
 const TabPane = Tabs.TabPane;
 
-const topFields: NftRecordDetailField[] = [
-  { name: "mintAddress", label: "Mint Address" },
-  { name: "description", label: "Description" },
+const fieldGroups: NftRecordDetailFieldGroup[] = [
+  {
+    name: 'Project Info',
+    fields: [
+      { name: 'projectName', label: 'Project' },
+      { name: 'auditor', label: 'Auditor' },
+      { name: 'projectStartDate', label: 'Start Date' },
+      { name: 'mintAddress', label: 'Mint to' },
+      { name: 'country', label: 'Country' },
+      { name: 'state', label: 'State' },
+      { name: 'registerId', label: 'Registry Id' },
+      { name: 'registryURL', label: 'Registry Link' },
+      { name: 'issuanceDate', label: 'Issuance Date' },
+      { name: 'ghgReduction', label: 'GHG Reduction' },
+      { name: 'trancheCutoff', label: 'Tranche Cutoff' },
+    ],
+  },
+  {
+    name: 'Credit Info',
+    fields: [
+      { name: 'creditsPre2020', label: 'Pre 2020 Credits' },
+      { name: 'credits2020', label: '2020 Project Credits' },
+      { name: 'credits2021', label: '2021 Project Credits' },
+      { name: 'credits2022', label: '2022 Project Credits' },
+      { name: 'credits2023', label: '2023 Project Credits' },
+      { name: 'credits2024', label: '2024 Project Credits' },
+      { name: 'existingCredits', label: 'Existing Carbon Credits' },
+      {
+        name: 'estimatedEmissionsReduction',
+        label: 'Estimated Annual Emissions Reduction',
+      },
+    ],
+  },
+  {
+    name: 'Pricing Information',
+    fields: [
+      { name: 'price', label: 'Price per Credit' },
+      { name: 'totalPrice', label: 'Total Price' },
+    ],
+  },
 ];
 
-const fields: NftRecordDetailField[] = [
-  { name: "nftTitle", label: "Name" },
-  { name: "trancheCutoff", label: "Tranche Cutoff" },
-  { name: "projectName", label: "Project/Site Name" },
-  { name: "registerId", label: "Registry Id" },
-  { name: "mintAddress", label: "Mint to" },
-  { name: "auditor", label: "Auditor" },
-  { name: "carbonAmount", label: "Carbon Amount" },
-  { name: "issuanceDate", label: "Issuance Date" },
-  { name: "description", label: "Description" },
-  { name: "carbonAmount", label: "Carbon Amount" },
-  { name: "issuingEntity", label: "Issuing Entity" },
-  { name: "description", label: "Description" },
-];
+interface ClaimTopic {
+  key: string;
+  displayName: string;
+  id: string;
+  topic: string;
+}
 
 interface NftRecordDetailProps {
   TablesData?: TableData[];
   handleMint?: () => void;
   handleBack?: () => void;
   data: {
-    [key: string]: string | string[];
+    [key: string]: string | string[] | ClaimTopic[];
   };
   detailView?: boolean;
 }
+
+const generateSvgIcon = (color: string) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='100%'
+      height='100%'
+      viewBox='0 0 560 560'>
+      <defs>
+        <linearGradient
+          id={`gradient-${color}`}
+          x1='0%'
+          y1='0%'
+          x2='0%'
+          y2='100%'>
+          <stop offset='0%' stopColor={color} stopOpacity='1' />
+          <stop offset='100%' stopColor='#003366' stopOpacity='1' />
+        </linearGradient>
+      </defs>
+      <rect width='560' height='560' rx='15' fill={`url(#gradient-${color})`} />
+      <text
+        x='50%'
+        y='50%' // Adjusted to bring text to vertical center
+        fontFamily='Arial, sans-serif'
+        fontWeight='bold'
+        fontSize='300' // Increased font size to make "KC" bigger
+        fill='white'
+        dominantBaseline='middle'
+        textAnchor='middle'>
+        KC
+      </text>
+    </svg>
+  );
+};  
 
 const NftRecordDetail = ({
   TablesData = [],
@@ -45,71 +109,90 @@ const NftRecordDetail = ({
   data,
   detailView = false,
 }: NftRecordDetailProps) => {
+  console.log(data);
+  const color = hashToColor(`${data['nftTitle'] as string}`);
   const router = useRouter();
   const { transactionHash } = data;
-  const title = detailView ? `NBT - ${data["nftTitle"]}` : "Preview NBT</>";
+  const title = detailView
+    ? `Carbon Credit - ${data['nftTitle']}`
+    : 'Preview Carbon Credit Token </>';
   const backButton = (
-    <Button onClick={() => router.back()} type="text" icon={<LeftOutlined />} />
+    <Button onClick={() => router.back()} type='text' icon={<LeftOutlined />} />
   );
 
   console.log(data);
 
   const tokenDetails = (
-    <Card className="bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark border-nomyx-gray4-light dark:border-nomyx-gray4-dark text-nomyx-text-light dark:text-nomyx-text-dark">
-      {fields.map((field: NftRecordDetailField, index: number) => (
-        <Card.Grid
-          key={`field-${index}`}
-          className="grid-cols-1 overflow-hidden text-ellipsis"
-        >
-          <div className="card-label">{field.label}</div>
-          <div className="card-value truncate">{data[field.name]}</div>
-        </Card.Grid>
+    <div className='space-y-4 mb-2'>
+      {fieldGroups.map((fieldGroup, index) => (
+        <div key={`group-${index}`}>
+          <div className='p-2 font-bold'>{fieldGroup.name}</div>
+          <div className='grid grid-cols-2 border-t border-b border-nomyx-gray4-light dark:border-nomyx-gray4-dark'>
+            {fieldGroup.fields.map(
+              (field: NftRecordDetailField, index: number) => (
+                <>
+                  {data[field.name] && (
+                    <div
+                      key={`field-${index}`}
+                      className='p-2 border-b odd:border-r last:border-0 odd:[&:nth-last-child(2)]:border-b-0  border-nomyx-gray4-light dark:border-nomyx-gray4-dark'>
+                      <div className='text-nomyx-gray3-light dark:text-nomyx-gray3-dark'>
+                        {field.label}
+                      </div>
+                      <div className='card-value truncate'>
+                        {data[field.name] as string}
+                      </div>
+                    </div>
+                  )}
+                </>
+              )
+            )}
+          </div>
+        </div>
       ))}
-    </Card>
+    </div>
   );
 
   return (
     <>
       <Card
-        className="bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark border-nomyx-gray4-light dark:border-nomyx-gray4-dark text-nomyx-text-light dark:text-nomyx-text-dark"
+        className='bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark border-nomyx-gray4-light dark:border-nomyx-gray4-dark text-nomyx-text-light dark:text-nomyx-text-dark'
         title={detailView ? [backButton, title] : title}
+        styles={{ body: { padding: '0' } }}
         extra={
           <Button
             onClick={() =>
               window.open(
                 `https://sepolia.etherscan.io/tx/${
-                  transactionHash ? transactionHash : ""
+                  transactionHash ? transactionHash : ''
                 }`
               )
             }
-            className="flex items-center gap-2 text-nomyx-text-light dark:text-nomyx-text-dark hover:!bg-transparent"
-          >
+            className='flex items-center gap-2 text-nomyx-text-light dark:text-nomyx-text-dark hover:!bg-transparent'>
             <ShareIcon />
             View On Block Explorer
           </Button>
-        }
-      >
-        <div className="grid grid-cols-3 gap-4">
-          {topFields.map((field: NftRecordDetailField, index: number) => (
-            <div
-              key={`field-${index}`}
-              className="grid-cols-1 overflow-hidden text-ellipsis font-[1.3em]"
-            >
-              <p className="text-xs">
-                <span className="card-label">{field.label}</span>
-              </p>
-              <div className="truncate">
-                <span className="card-value text-sm">{data[field.name]}</span>
-              </div>
+        }>
+        <div className='flex flex-col mb-4'>
+          <div className='font-bold p-2'>Details</div>
+          <div className='flex gap-4 px-2'>
+            <div className='flex items-center justify-center h-52 w-52'>
+              {generateSvgIcon(color)}
             </div>
-          ))}
+            <div className='p-4 py-8'>
+              <h1 className='text-3xl font bold'>
+                {data['nftTitle'] as string}
+              </h1>
+              <p className='!text-nomyx-gray1-light dark:!text-nomyx-gray1-dark'>
+                {data['description'] as string}
+              </p>
+            </div>
+          </div>
         </div>
-      </Card>
-      <Card styles={{ body: { padding: "0" } }}>
+
         {detailView && (
           <>
-            <Tabs defaultActiveKey="1">
-              <TabPane tab="Token Details" key="1">
+            <Tabs defaultActiveKey='1'>
+              <TabPane tab='Token Details' key='1'>
                 {tokenDetails}
               </TabPane>
 
@@ -119,26 +202,25 @@ const NftRecordDetail = ({
                     key={index + 2}
                     tab={
                       <>
-                        <div className="mr-2 inline-block">
+                        <div className='mr-2 inline-block'>
                           {React.isValidElement(tableData.headerImage) ? (
                             tableData.headerImage
                           ) : (
                             <Image
-                              alt=""
+                              alt=''
                               src={tableData.headerImage as string}
                               height={10}
                               width={20}
-                              className="inline-block"
+                              className='inline-block'
                             />
                           )}
                         </div>
 
                         {tableData.label}
                       </>
-                    }
-                  >
+                    }>
                     <Table
-                      rowKey="id"
+                      rowKey='id'
                       dataSource={tableData.tableData}
                       columns={tableData.columns}
                     />
@@ -150,36 +232,39 @@ const NftRecordDetail = ({
         )}
 
         {!detailView && tokenDetails}
-      </Card>
-
-      <Card title="About Kronos Tokens" className="bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark border-nomyx-gray4-light dark:border-nomyx-gray4-dark text-nomyx-text-light dark:text-nomyx-text-dark">
-        <p>Kronos Yield Generating NBT</p>
-        <p className="text-[#871212]">{`This ra-NBTI asset (Note-Backed Token) entitles you to participation rights in Loan#${
-          data?.loanId || ""
-        } with an origination value of $${
-          data?.loanAmount || ""
-        } with lender qualifier of ${
-          data?.ficoScore || ""
-        } FICO ranking. generating an annual yield ${
-          data?.yield || ""
-        }.It provides a total payout of $${
-          data?.loanAmount || ""
-        } over a term of ${data?.term || ""} months.`}</p>
+        {!detailView && (
+          <>
+            <div className='p-2 font-bold'>Compliance Features</div>
+            <div className='mb-2 p-2 border-t border-nomyx-gray4-light dark:border-nomyx-gray4-dark'>
+              {(data['claimTopics'] as ClaimTopic[])
+                .filter((topic) =>
+                  (data['targetKeys'] as string[]).includes(topic['key'])
+                )
+                .map((topic, index) => (
+                  <div key={`claim-${index}`}>
+                    <Checkbox defaultChecked disabled>
+                      <p>
+                        {topic.displayName} ({topic.topic})
+                      </p>
+                    </Checkbox>
+                  </div>
+                ))}
+            </div>
+          </>
+        )}
       </Card>
 
       {!detailView && (
         <>
-          <div className="w-full flex justify-end gap-4 pt-2">
+          <div className='w-full flex justify-end gap-4 pt-2'>
             <Button
-              className="text-nomyx-text-light dark:text-nomyx-text-dark hover:!bg-transparent"
-              onClick={handleBack}
-            >
+              className='text-nomyx-text-light dark:text-nomyx-text-dark hover:!bg-transparent'
+              onClick={handleBack}>
               Back
             </Button>
             <Button
-              className="text-nomyx-text-light dark:text-nomyx-text-dark hover:!bg-transparent"
-              onClick={handleMint}
-            >
+              className='bg-nomyx-blue-light mr-4 hover:!bg-nomyx-dark1-light hover:dark:!bg-nomyx-dark1-dark'
+              onClick={handleMint}>
               Mint
             </Button>
           </div>
