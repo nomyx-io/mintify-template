951794c5a52982fa0d416709dc0b1bbc4e5dd2f2
Justina <justinamary27@gmail.com>
2024-10-11 20:30:48 +0530

    fixed minor UI issues

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -21,15 +21,15 @@ export default function Home() {
         api.getEvents(),
         api.getKpis(),
       ]);
-      
+
       setTokenGraphValues({
         labels: ["Total Tokens Issued", "Total Tokens Retired"],
         values: [kpis.tokens, kpis.retired],
-      })
+      });
       setCarbonGraphValues({
         labels: ["Total Carbon Issued", "Total Carbon Retired"],
         values: [kpis.carbonIssued, kpis.carbonRetired],
-      })
+      });
       setkpisData(kpis);
       setEventDetails(events || {});
     } catch (error) {
@@ -43,35 +43,45 @@ export default function Home() {
 
   const items = [
     {
-      label: 'Token Insights',
-      key: '1',
+      label: "Token Insights",
+      key: "1",
       children: (
-        <BarChart data={getGraphData(tokenGraphValues)} title='Net Token Issued & Redeemed' />
+        <BarChart
+          data={getGraphData(tokenGraphValues)}
+          title="Net Token Issued & Redeemed"
+        />
       ),
     },
     {
-      label: 'Carbon Insights',
-      key: '2',
+      label: "Carbon Insights",
+      key: "2",
       children: (
-        <BarChart data={getGraphData(carbonGraphValues)} title='Net Carbon Issued & Redeemed' />
+        <BarChart
+          data={getGraphData(carbonGraphValues)}
+          title="Net Carbon Issued & Redeemed"
+        />
       ),
     },
   ];
 
   return (
-    <div className="w-full grid grid-cols-1 lg:grid-cols-4 gap-3">
-      <div className="lg:col-span-3">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-4 gap-3 h-[100%]">
+      <div className="lg:col-span-3 flex flex-col left">
         <div className="flex lg:grid grid-cols-3 gap-3 pb-3 flex-wrap">
           {getKPIs(kpisData)?.map((kpi) => (
-            <KPI key={kpi.title} icon={kpi.icon} title={kpi.title} value={kpi.value} />
+            <KPI
+              key={kpi.title}
+              icon={kpi.icon}
+              title={kpi.title}
+              value={kpi.value}
+            />
           ))}
         </div>
-
-        <Card className="w-full flex-grow no-padding bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark border-nomyx-gray4-light dark:border-nomyx-gray4-dark">
+        <Card className="w-full flex-grow no-padding bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark border-nomyx-gray4-light dark:border-nomyx-gray4-dark flex-1">
           <Tabs items={items}></Tabs>
         </Card>
       </div>
-      <Card className="no-padding h-[90vh] lg:max-w-sm overflow-y-auto border-nomyx-gray4-light dark:border-nomyx-gray4-dark bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark">
+      <Card className="no-padding h-full overflow-y-auto border-nomyx-gray4-light dark:border-nomyx-gray4-dark bg-nomyx-dark2-light dark:bg-nomyx-dark2-dark right">
         <EventFeed data={eventDetails} />
       </Card>
     </div>
