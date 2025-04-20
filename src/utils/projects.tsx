0f1a1b7ead3878593c9cdc33270276b33b5c1da7
hashdive <nile.sarkisian@gmail.com>
2025-04-20 10:45:07 -0400

    event name spacing/ remove underscore from token columns names

--- src/utils/projects.tsx
+++ src/utils/projects.tsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 
 import { formatPrice } from "./currencyFormater";
 
+function formatTitle(label: string) {
+  return label
+    .replace(/_/g, " ") // Replace underscores with spaces
+    .replace(/\b\w/g, (c) => c.toUpperCase()); // Capitalize first letter of each word
+}
+
 export const projectColumns: any = (onProjectClick: (project: Project) => void) => [
   {
     title: "Title",
@@ -12,42 +18,36 @@ export const projectColumns: any = (onProjectClick: (project: Project) => void) 
       multiple: 1,
     },
     width: 300,
-    render: (project: Project) => {
-      return (
-        <div className="flex items-center">
-          <div className="flex justify-center items-center pr-4 border-r">
-            {/* Eye Icon triggers onProjectClick */}
-            <Eye className="text-xl cursor-pointer hover:text-blue-500" onClick={() => onProjectClick(project)} />
-          </div>
-          <div className="w-10 h-10 relative rounded overflow-hidden flex-shrink-0 ml-4">
-            <Image src={project.logo?.url() || "/default-image.png"} alt={project.title} fill className="object-cover" />
-          </div>
-          <h2 className="ml-4 font-semibold">{project.title}</h2>
+    render: (project: Project) => (
+      <div className="flex items-center">
+        <div className="flex justify-center items-center pr-4 border-r">
+          <Eye className="text-xl cursor-pointer hover:text-blue-500" onClick={() => onProjectClick(project)} />
         </div>
-      );
-    },
+        <div className="w-10 h-10 relative rounded overflow-hidden flex-shrink-0 ml-4">
+          <Image src={project.logo?.url() || "/default-image.png"} alt={project.title} fill className="object-cover" />
+        </div>
+        <h2 className="ml-4 font-semibold">{project.title}</h2>
+      </div>
+    ),
   },
   {
     dataIndex: "description",
-    title: "Description",
+    title: formatTitle("description"),
     align: "left",
     width: 400,
     ellipsis: true,
-    // No sorter for Description
   },
   {
-    title: "Total Tokens",
+    title: formatTitle("totalTokens"),
     dataIndex: "totalTokens",
     width: 150,
     sorter: (a: any, b: any) => a.totalTokens - b.totalTokens,
   },
   {
-    title: "Total Token Value",
+    title: formatTitle("totalValue"),
     dataIndex: "totalValue",
     width: 300,
-    render: (totalValue: number) => {
-      formatPrice(totalValue, "USD");
-    },
+    render: (totalValue: number) => formatPrice(totalValue, "USD"),
     sorter: (a: any, b: any) => a.totalValue - b.totalValue,
   },
 ];
